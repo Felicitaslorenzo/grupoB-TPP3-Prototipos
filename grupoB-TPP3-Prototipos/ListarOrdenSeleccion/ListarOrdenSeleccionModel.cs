54b23a5a9436dd0ffae81d05548481cf1d5c7595
KEYTHI CONSUELO LUNA VICTORIA POLONIO <34LU94814609@campus.economicas.uba.ar>
2024-11-11 18:01:11 -0300

    Listo pantalla ListarOS

--- grupoB-TPP3-Prototipos/ListarOrdenSeleccion/ListarOrdenSeleccionModel.cs
+++ grupoB-TPP3-Prototipos/ListarOrdenSeleccion/ListarOrdenSeleccionModel.cs
@@ -12,28 +12,29 @@ namespace grupoB_TPP3_Prototipos.ListarOrdenSeleccion
     {
         public List<OrdenSeleccion> OrdenesSeleccionadas = OrdenSeleccionAlmacen.OrdenesSeleccion.Select(os => new OrdenSeleccion
         {
-                IdOrdenSeleccion = os.IdOrdenSeleccion,
-                FechaEmision = os.FechaEmision,
-                FechaEstado = os.FechaEstado,
-                Estado = os.Estado.ToString(),
+            IdOrdenSeleccion = os.IdOrdenSeleccion,
+            FechaEmision = os.FechaEmision,
+            FechaEstado = os.FechaEstado,
+            Estado = os.Estado.ToString(),
 
-                // Obtiene las órdenes de preparación asociadas para cada orden de selección.
-                OrdenesPreparacion = OrdenPreparacionAlmacen.OrdenesPreparacion
-                .Where(op => op.IdOrdenPreparacion == os.IdOrdenSeleccion)
-                .Select(op => new OrdenPreparacion
-                {
-                    IdOrden = op.IdOrdenPreparacion,
-                    IdCliente = op.IdCliente,
-                    Transportista = op.IdTransportista,
+            // Obtiene las órdenes de preparación asociadas para cada orden de selección.
+            OrdenesPreparacion = OrdenPreparacionAlmacen.OrdenesPreparacion
+         .Where(op => os.OrdenesPreparacion.Contains(op.IdOrdenPreparacion))  // Usar Contains para la comparación
+         .Select(op => new OrdenPreparacion
+         {
+             IdOrden = op.IdOrdenPreparacion,
+             IdCliente = op.IdCliente,
+             Transportista = op.IdTransportista,
 
-                    // Obtiene la lista de productos de cada orden de preparación.
-                     Productos = op.Detalle.Select(det => new Producto
-                     {
-                         DescripcionProducto = ProductoAlmacen.Productos
-                                          .First(p => p.SKUProducto == det.SKUProducto).DescripcionProducto,
-                         Cantidad = det.Cantidad
-                     }).ToList()
-                }).ToList()
+             // Obtiene la lista de productos de cada orden de preparación.
+             Productos = op.Detalle.Select(det => new Producto
+             {
+                 DescripcionProducto = ProductoAlmacen.Productos
+                     .First(p => p.SKUProducto == det.SKUProducto).DescripcionProducto,
+                 Cantidad = det.Cantidad
+             }).ToList()
+         }).ToList()
         }).ToList();
+
     }
 }
