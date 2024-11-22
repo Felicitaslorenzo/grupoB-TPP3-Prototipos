966e67bc842eea48cb9f8d11f1bd2f8036240ec3
KEYTHI CONSUELO LUNA VICTORIA POLONIO <34LU94814609@campus.economicas.uba.ar>
2024-11-22 01:24:18 -0300

    Listo Empaquetar

--- grupoB-TPP3-Prototipos/Empaquetado/EmpaquetadoForm.Designer.cs
+++ grupoB-TPP3-Prototipos/Empaquetado/EmpaquetadoForm.Designer.cs
@@ -34,14 +34,15 @@
             Cantidad = new ColumnHeader();
             VolverButton = new Button();
             label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // EmpaquetarOrdenButton
             // 
-            EmpaquetarOrdenButton.Location = new Point(549, 307);
+            EmpaquetarOrdenButton.Location = new Point(519, 417);
             EmpaquetarOrdenButton.Margin = new Padding(1);
             EmpaquetarOrdenButton.Name = "EmpaquetarOrdenButton";
-            EmpaquetarOrdenButton.Size = new Size(168, 43);
+            EmpaquetarOrdenButton.Size = new Size(168, 51);
             EmpaquetarOrdenButton.TabIndex = 58;
             EmpaquetarOrdenButton.Text = "Empaquetado";
             EmpaquetarOrdenButton.UseVisualStyleBackColor = true;
@@ -50,7 +51,7 @@
             // ListarEmpaquetarOrdenList
             // 
             ListarEmpaquetarOrdenList.Columns.AddRange(new ColumnHeader[] { Producto, Cantidad });
-            ListarEmpaquetarOrdenList.Location = new Point(23, 15);
+            ListarEmpaquetarOrdenList.Location = new Point(20, 116);
             ListarEmpaquetarOrdenList.Name = "ListarEmpaquetarOrdenList";
             ListarEmpaquetarOrdenList.Size = new Size(845, 269);
             ListarEmpaquetarOrdenList.TabIndex = 50;
@@ -69,10 +70,10 @@
             // 
             // VolverButton
             // 
-            VolverButton.Location = new Point(738, 305);
+            VolverButton.Location = new Point(736, 417);
             VolverButton.Margin = new Padding(1);
             VolverButton.Name = "VolverButton";
-            VolverButton.Size = new Size(129, 45);
+            VolverButton.Size = new Size(129, 51);
             VolverButton.TabIndex = 48;
             VolverButton.Text = "Volver";
             VolverButton.UseVisualStyleBackColor = true;
@@ -81,17 +82,29 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(23, 318);
+            label1.Font = new Font("Century", 12F);
+            label1.Location = new Point(20, 71);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(65, 23);
             label1.TabIndex = 59;
             label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Century Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(20, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(322, 34);
+            label2.TabIndex = 60;
+            label2.Text = "Empaquetar Productos\r\n";
             // 
             // EmpaquetadoForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(888, 368);
+            ClientSize = new Size(888, 498);
+            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(EmpaquetarOrdenButton);
             Controls.Add(ListarEmpaquetarOrdenList);
@@ -112,5 +125,6 @@
         private ColumnHeader Producto;
         private ColumnHeader Cantidad;
         private Label label1;
+        private Label label2;
     }
 }
