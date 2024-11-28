81f1f4b6fddd441d75344000706787b76971758a
KEYTHI CONSUELO LUNA VICTORIA POLONIO <34LU94814609@campus.economicas.uba.ar>
2024-11-28 01:07:34 -0300

    Actu de diseño

--- grupoB-TPP3-Prototipos/ConsultaStock/ConsultaStockForm.Designer.cs
+++ grupoB-TPP3-Prototipos/ConsultaStock/ConsultaStockForm.Designer.cs
@@ -57,30 +57,27 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(18, 29);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(14, 26);
             label1.Name = "label1";
-            label1.Size = new Size(122, 25);
+            label1.Size = new Size(100, 20);
             label1.TabIndex = 0;
             label1.Text = "SKU Producto";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(388, 29);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(375, 23);
             label2.Name = "label2";
-            label2.Size = new Size(182, 25);
+            label2.Size = new Size(151, 20);
             label2.TabIndex = 1;
             label2.Text = "Descripción Producto";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(761, 29);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(726, 26);
             label3.Name = "label3";
-            label3.Size = new Size(88, 25);
+            label3.Size = new Size(74, 20);
             label3.TabIndex = 2;
             label3.Text = "ID Cliente";
             // 
@@ -88,38 +85,34 @@
             // 
             comboSKUProducto.DropDownStyle = ComboBoxStyle.DropDownList;
             comboSKUProducto.FormattingEnabled = true;
-            comboSKUProducto.Location = new Point(18, 61);
-            comboSKUProducto.Margin = new Padding(4);
+            comboSKUProducto.Location = new Point(14, 49);
             comboSKUProducto.Name = "comboSKUProducto";
-            comboSKUProducto.Size = new Size(320, 33);
+            comboSKUProducto.Size = new Size(289, 28);
             comboSKUProducto.TabIndex = 3;
             // 
             // comboDescripcionProducto
             // 
             comboDescripcionProducto.DropDownStyle = ComboBoxStyle.DropDownList;
             comboDescripcionProducto.FormattingEnabled = true;
-            comboDescripcionProducto.Location = new Point(388, 61);
-            comboDescripcionProducto.Margin = new Padding(4);
+            comboDescripcionProducto.Location = new Point(375, 49);
             comboDescripcionProducto.Name = "comboDescripcionProducto";
-            comboDescripcionProducto.Size = new Size(320, 33);
+            comboDescripcionProducto.Size = new Size(289, 28);
             comboDescripcionProducto.TabIndex = 4;
             // 
             // comboCliente
             // 
             comboCliente.DropDownStyle = ComboBoxStyle.DropDownList;
             comboCliente.FormattingEnabled = true;
-            comboCliente.Location = new Point(761, 61);
-            comboCliente.Margin = new Padding(4);
+            comboCliente.Location = new Point(726, 49);
             comboCliente.Name = "comboCliente";
-            comboCliente.Size = new Size(323, 33);
+            comboCliente.Size = new Size(289, 28);
             comboCliente.TabIndex = 5;
             // 
             // buttonBuscar
             // 
-            buttonBuscar.Location = new Point(932, 136);
-            buttonBuscar.Margin = new Padding(4);
+            buttonBuscar.Location = new Point(893, 109);
             buttonBuscar.Name = "buttonBuscar";
-            buttonBuscar.Size = new Size(152, 50);
+            buttonBuscar.Size = new Size(122, 40);
             buttonBuscar.TabIndex = 6;
             buttonBuscar.Text = "Buscar";
             buttonBuscar.UseVisualStyleBackColor = true;
@@ -130,10 +123,9 @@
             listProducto.BackColor = SystemColors.InactiveBorder;
             listProducto.Columns.AddRange(new ColumnHeader[] { columnSKUProducto, columnDescripcionProducto, columnCliente, columnTotal, columnCantComprometida, TotalDisponible });
             listProducto.FullRowSelect = true;
-            listProducto.Location = new Point(26, 294);
-            listProducto.Margin = new Padding(4);
+            listProducto.Location = new Point(21, 235);
             listProducto.Name = "listProducto";
-            listProducto.Size = new Size(1066, 345);
+            listProducto.Size = new Size(1001, 277);
             listProducto.TabIndex = 7;
             listProducto.UseCompatibleStateImageBehavior = false;
             listProducto.View = View.Details;
@@ -173,10 +165,9 @@
             // 
             listDetalle.Columns.AddRange(new ColumnHeader[] { columnDeposito, columnUbicacion, columnCantidad });
             listDetalle.FullRowSelect = true;
-            listDetalle.Location = new Point(26, 662);
-            listDetalle.Margin = new Padding(4);
+            listDetalle.Location = new Point(21, 530);
             listDetalle.Name = "listDetalle";
-            listDetalle.Size = new Size(1066, 345);
+            listDetalle.Size = new Size(1001, 277);
             listDetalle.TabIndex = 8;
             listDetalle.UseCompatibleStateImageBehavior = false;
             listDetalle.View = View.Details;
@@ -198,10 +189,9 @@
             // 
             // buttonVolver
             // 
-            buttonVolver.Location = new Point(941, 1039);
-            buttonVolver.Margin = new Padding(4);
+            buttonVolver.Location = new Point(900, 829);
             buttonVolver.Name = "buttonVolver";
-            buttonVolver.Size = new Size(152, 50);
+            buttonVolver.Size = new Size(122, 40);
             buttonVolver.TabIndex = 9;
             buttonVolver.Text = "Volver";
             buttonVolver.UseVisualStyleBackColor = true;
@@ -209,10 +199,9 @@
             // 
             // buttonLimpiar
             // 
-            buttonLimpiar.Location = new Point(701, 136);
-            buttonLimpiar.Margin = new Padding(4);
+            buttonLimpiar.Location = new Point(726, 109);
             buttonLimpiar.Name = "buttonLimpiar";
-            buttonLimpiar.Size = new Size(152, 50);
+            buttonLimpiar.Size = new Size(122, 40);
             buttonLimpiar.TabIndex = 10;
             buttonLimpiar.Text = "Limpiar";
             buttonLimpiar.UseVisualStyleBackColor = true;
@@ -222,10 +211,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Century Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(15, 11);
-            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Location = new Point(12, 9);
             label4.Name = "label4";
-            label4.Size = new Size(267, 39);
+            label4.Size = new Size(226, 34);
             label4.TabIndex = 83;
             label4.Text = "Consultar Stock\r\n";
             // 
@@ -239,19 +227,17 @@
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(9, 58);
-            groupBox1.Margin = new Padding(4);
+            groupBox1.Location = new Point(7, 46);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new Padding(4);
-            groupBox1.Size = new Size(1101, 206);
+            groupBox1.Size = new Size(1032, 165);
             groupBox1.TabIndex = 84;
             groupBox1.TabStop = false;
             // 
             // ConsultaStockForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1120, 1050);
+            ClientSize = new Size(1045, 878);
             Controls.Add(groupBox1);
             Controls.Add(label4);
             Controls.Add(buttonVolver);
@@ -259,7 +245,6 @@
             Controls.Add(listProducto);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(4);
             Name = "ConsultaStockForm";
             StartPosition = FormStartPosition.CenterScreen;
             Load += ConsultaStockForm_Load;
